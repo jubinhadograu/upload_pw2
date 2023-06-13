5e3398b834437c04190ce9c52e33949b4649be77
jubinhadograu <juliasrly@gmail.com>
2023-06-12 22:22:49 -0300

    configuração do multer e upload de imagens

--- index.js
+++ index.js
@@ -1,23 +1,50 @@
 const express = require('express');
+const multer = require('multer');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
 
-//CONFIGURAÇÃO DE STORAGE
+/*CONFIGURAÇÃO DE STORAGE: onde e como será gravado*/
 const storage = multer.diskStorage(
     {
         destination:(req, file, cb)=>{
-            cb(null, './uploads')
+            cb(null, './uploads');
         },
         filename:(req, file, cb)=>{
-            cb(null, Date.now().toString() + '_' + file.originalname)
+            cb(null, Date.now().toString() + '_' + file.originalname);
         }
     }
 );
 
-//CONFIGURAÇÃO DE FILTER
+/*CONFIGURAÇÃO DE FILTER: filtragem de arquivos*/
+const fileFilter = (req, file, cb) =>{
+    if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg') {
+cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
+
+/* UPLOAD */
+const upload = multer({
+    //regra de storage
+    storage:storage,
+    //regra de tamanho
+    limits:{
+        fileSize: 1024 * 1024 * 5
+    },
+    //regra de filter
+    fileFilter:fileFilter
+});
+
+/*ROTA DE UPLOAD DE ARQUIVO*/
+app.post('/upload', upload.array('imagens_livro', 2),(req, res) =>{
+    console.log(req.files);//upload dos arquivos de imagem
+    console.log(req.body);//upload dos arquivos de texto
+    res.send('OK!🤓');
+});
 
 app.listen(3000, ()=>{
     console.log('SERVIDOR RODANDO EM http://localhost:3000')
